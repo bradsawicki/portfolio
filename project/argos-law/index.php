84366bc4b19a882e6b4459bce912870b50fc111b
Brad Sawicki <git@bradsawicki.com>
2013-12-30 11:11:59 -0600

    BTN font update / Argos project image

--- project/argos-law/index.php
+++ project/argos-law/index.php
@@ -9,13 +9,13 @@
 			<h1 class="title">Argos Law</h1>
 			<div class="content">
 				<p class="description">Argos Law is a boutique law firm in Chicago. Law firm websites and their users follow some fairly strict rules and Argos Law needed a site that kept within the rules, while still promoting a friendly future modern design and interface.  Considering the target audience is on the the go, an adaptive site for all devices was of great importance. The interface was designed in browser and tested on different devices to fine tune it’s responsive design. The result is a beautiful interface for desktop, tablet and mobile users.</p>
-				<div class="role">
-				    <h4 class="label">Role</h4>
-				    <p>Responsive Design / Drupal Development (CMS)</p>
-				</div>
 			</div>
-			<div class="pic"><img src="/images/pic-desktop-argos.jpg" alt="Argos Law" /></div>
-			<div class="visit"><a href="http://argoslaw.com" class="btn blue" target="_blank">Visit Site</a></div>
+			<div class="pic"><img src="/images/pic-desktop-argos-law.jpg" alt="Argos Law" /></div>
+			<div class="role">
+			    <h4 class="label">Role</h4>
+			    <p>Responsive Design / Drupal Development (CMS)</p>
+			</div>
+			<div class="visit"><a href="http://argoslaw.com" target="_blank">Visit Site</a></div>
 			
 		</div>
 	</section>
