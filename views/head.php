25a873b259ccb6d08b488a89ce489a225b80b1d5
Brad Sawicki <git@bradsawicki.com>
2014-01-01 14:29:08 -0600

    Favicon

--- views/head.php
+++ views/head.php
@@ -8,6 +8,7 @@
 	<title><?php echo($title_tag); ?> | Brad Sawicki</title>
 	<link href="/css/fonts/icons.css" rel="stylesheet" media="all">
 	<link href="/css/styles.css" rel="stylesheet" media="all">
+	<link rel="shortcut icon" href="/images/favicon.png" />
 	<script type="text/javascript" src="//use.typekit.net/xbt5hzm.js"></script>
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 	<!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
