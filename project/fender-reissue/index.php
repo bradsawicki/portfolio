5a6863693e44e3d98cb885e33e741adca983deb1
Brad Sawicki <git@bradsawicki.com>
2014-01-28 17:15:44 -0600

    Add Fender Reissue Shop project page

---
+++ project/fender-reissue/index.php
@@ -0,0 +1,25 @@
+<?php $title_tag = "Fender Reissue"; ?>
+<?php $body_class = "project"; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/head.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/header.php'; ?>
+	
+	<section class="main">
+		<div class="wrapper cf">
+			
+			<h1 class="title">The Fender Reissue Shop</h1>
+			<p class="role">Branding, UI / UX, Web Design, Drupal Development</p>
+			<div class="pic"><img src="/images/pic-desktop-fender-reissue.jpg" alt="The Fender Reissue Shop" /></div>
+			<div class="content">
+				<p class="description">The Fender Reissue Shop deals Fender and Gretch guitars. They are the sister company of The Gibson Shop. Being one of my first freelance projects and a guitarist, this was an exciting project to be apart of. To embrace the vintage Fender vibe in the design, I utilized the textures from the classic tweed guitar case. Inventory updates and content management are powered by Drupal.</p>
+			</div>
+			<div class="project-nav">
+			    <a href="/project/cappellos/" class="arrow prev"></a>
+			    <a href="http://fenderreissue.com" target="_blank" class="visit">Visit Site <i class="icon"></i></a>
+			    <a href="/project/argoslaw/" class="arrow next"></a>
+			</div>
+			
+		</div>
+	</section>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/bottom.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/footer.php'; ?>
