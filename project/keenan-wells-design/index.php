bd671d96c9c84ed8f126224cee4774c06bfffa11
Brad Sawicki <git@bradsawicki.com>
2014-01-28 17:22:33 -0600

    Add Keenan Wells project page

---
+++ project/keenan-wells-design/index.php
@@ -0,0 +1,25 @@
+<?php $title_tag = "Keenan Wells Design"; ?>
+<?php $body_class = "project"; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/head.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/header.php'; ?>
+	
+	<section class="main">
+		<div class="wrapper cf">
+			
+			<h1 class="title">Keenan Wells Design</h1>
+			<p class="role">Front-end Development</p>
+			<div class="pic"><img src="/images/pic-desktop-keenan-wells.jpg" alt="Keenan Wells Design" /></div>
+			<div class="content">
+				<p class="description">Keenan Wells is a Chicago Web Designer, Illustrator and old friend of mine. We've collaborated on many web projects, so it was natural to help him out with the development of his portfolio site. We strayed from using a CMS and went with static site of good ol' hand written clean code.</p>
+			</div>
+			<div class="project-nav">
+			    <a href="/project/cappellos/" class="arrow prev"></a>
+			    <a href="http://fenderreissue.com" target="_blank" class="visit">Visit Site <i class="icon"></i></a>
+			    <a href="/project/argoslaw/" class="arrow next"></a>
+			</div>
+			
+		</div>
+	</section>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/bottom.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/footer.php'; ?>
