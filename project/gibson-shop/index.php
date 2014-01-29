1f5e1724ebf123b538c00525db06c093adb7ed17
Brad Sawicki <git@bradsawicki.com>
2014-01-28 18:27:49 -0600

    Gibson Shop img add

--- project/gibson-shop/index.php
+++ project/gibson-shop/index.php
@@ -1,4 +1,4 @@
-<?php $title_tag = "The Gibson SHop"; ?>
+<?php $title_tag = "The Gibson Shop"; ?>
 <?php $body_class = "project"; ?>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/views/head.php'; ?>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/views/header.php'; ?>
@@ -6,9 +6,9 @@
 	<section class="main">
 		<div class="wrapper cf">
 			
-			<h1 class="title">The Gibson SHop</h1>
+			<h1 class="title">The Gibson Shop</h1>
 			<p class="role">Branding, UI / UX, Responsive Web Design, Drupal Development</p>
-			<div class="pic"><img src="/images/pic-desktop-everygirl.jpg" alt="The Everygirl" /></div>
+			<div class="pic"><img src="/images/pic-desktop-gibson-shop.jpg" alt="The Gibson Shop" /></div>
 			<div class="content">
 				<p class="description">The Gibson Shop deals Gibson guitars online and is the sister company of The Fender Reissue Shop. After I completed work on the Fender Reissue Shop’s site, they came to me in need of branding and a new site for The Gibson Shop that would allow for easy inventory management. To achieve this, Drupal was used for content management, giving them complete control of their site.</p>
 			</div>
