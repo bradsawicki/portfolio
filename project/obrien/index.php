84057cab6d33e9be5c98ab2983e732650e82495f
Brad Sawicki <git@bradsawicki.com>
2014-02-02 12:36:10 -0600

    Add Obrien Project

---
+++ project/obrien/index.php
@@ -0,0 +1,25 @@
+<?php $title_tag = "OBRIEN"; ?>
+<?php $body_class = "project"; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/head.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/header.php'; ?>
+	
+	<section class="main">
+		<div class="wrapper cf">
+			
+			<h1 class="title">Obrien</h1>
+			<p class="role">UI / UX, Responsive Web Design, Drupal Development</p>
+			<div class="pic"><img src="/images/pic-desktop-obrien.jpg" alt="Obrien" /></div>
+			<div class="content">
+				<p class="description">OBRIEN was in need of a fresh site to embrace their new technologies and message. Drupal was used to give them complete control of the sites content. I took the responsive web design approach to deliver an optimal experience to desktop, tablet and mobile users.</p>
+			</div>
+			<div class="project-nav">
+			    <a href="/project/cappellos/" class="arrow prev"></a>
+			    <a href="http://thegibsonshop.com" target="_blank" class="visit">Visit Site <i class="icon"></i></a>
+			    <a href="/project/fourkites/" class="arrow next"></a>
+			</div>
+			
+		</div>
+	</section>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/bottom.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/footer.php'; ?>
