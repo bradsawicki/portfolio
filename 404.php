9232de4038e5ebdb20c3c7d96ad3a8f2a7b7ec12
Brad Sawicki <git@bradsawicki.com>
2014-02-02 12:59:35 -0600

    404 page

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php $title_tag = "404"; ?>
+<?php $body_class = "project page-404"; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/head.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/header.php'; ?>
+	
+	<section class="main">
+		<div class="wrapper cf">
+			
+			<h1 class="title">Nope!</h1>
+			<div class="content">
+				<p>How the hell did you get here? Call your mother right now and tell her you love her. Do it! And remember, condoms don't protect the heart.</p>
+				<p>Please go to the home page and take it from there, boss.</p>
+			</div>
+			<div class="project-nav">
+			    <a href="/" class="visit">Home Page <i class="icon"></i></a>
+			</div>
+			
+		</div>
+	</section>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/bottom.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/footer.php'; ?>
